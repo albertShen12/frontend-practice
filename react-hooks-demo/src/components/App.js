34b94b5589aacd109cadff073c432ab28d84e654
albert.shen <albert.shen@foxmail.com>
2019-12-09 21:44:59 +0800

    little change

--- react-hooks-demo/src/components/App.js
+++ react-hooks-demo/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../logo.svg';
 import '../stlyes/App.less';
+import Header from './Header';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Header text={"albert"}/>
         <a
           className="App-link"
           href="https://reactjs.org"
