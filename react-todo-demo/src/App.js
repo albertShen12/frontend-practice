9467bdcd4713dc2c2667b7527a08fa5bf7987927
albert.shen <albert.shen@foxmail.com>
2020-03-02 12:30:43 +0800

    add antd

--- react-todo-demo/src/App.js
+++ react-todo-demo/src/App.js
@@ -9,9 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p><Button type="primary">click me!</Button></p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
