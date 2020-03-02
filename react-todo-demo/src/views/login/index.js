47c3f4bf1bed0471d32da0c0796e8a81a9190326
albert.shen <albert.shen@foxmail.com>
2020-03-02 16:11:20 +0800

    add router 1 1

--- react-todo-demo/src/views/login/index.js
+++ react-todo-demo/src/views/login/index.js
@@ -1,9 +1,20 @@
 import React from "react";
-import {withRouter} from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import { Button } from "antd";
 
+const Login = props => {
+  let handleGoHome = () => {
+    props.history.push("/home");
+  };
 
-const Login=()=>{
-    return (<div>login</div>);
-}
+  return (
+    <div>
+      login
+      <p>
+        <Button type="primary" onClick={handleGoHome}>前面home页</Button>
+      </p>
+    </div>
+  );
+};
 
-export default withRouter(Login)
+export default withRouter(Login);
